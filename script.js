24211838d496789470a9b237cbaf68951367532c
Gabriel Caravantes <107145434+gabrielcaravantes@users.noreply.github.com>
2022-10-27 16:16:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const tbody = document.querySelector("tbody");
+const descItem = document.querySelector("#desc");
+const amount = document.querySelector("#amount");
+const type = document.querySelector("#type");
+const btnNew = document.querySelector("#btnNew");
+
+const incomes = document.querySelector(".incomes");
+const expenses = document.querySelector(".expenses");
+const total = document.querySelector(".total");
+
+let items;
+
+function insertItem(item, index) {
+    let tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td>${item.desc}</td>
+    <td>R$ ${item.amount}</td>
+    <td class="columnType">${
+        item.type === "Entrada"
+        ? '<i class="bx bxs-chevron-up-circle"></i>'
+        : '<i class="bx bxs-chevron-down-circle"></i>'
+    }</td>
+    <td class="columnAction">
+        <button onclick="deleteItem(${index})"><i class='bx-bx-trash'></i></button>
+        </td>
+    `
+}
+
+function loadItens() {
+    items = getItensBD();
+    tbody.innerHTML = "";
+    items.forEach((item, index) => {
+        insertItem(item, index);
+    });
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem("db_items")) ?? [];
+const setItensBD = () => localStorage.setItem("db_items", JSON.stringify(items));
+
+loadItens();
