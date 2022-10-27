10ca9b29e502c997b9a262645016a5e1b589e348
Gabriel Caravantes <107145434+gabrielcaravantes@users.noreply.github.com>
2022-10-27 16:39:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,84 +10,86 @@ const total = document.querySelector(".total");
 
 let items;
 
-btnNew.onClick = () => {
-    if (descItem.value === "" || amount.value === "" || type.value === ""){
-        return alert("é necessário preencher todos os campos!");
-    }
+btnNew.onclick = () => {
+  if (descItem.value === "" || amount.value === "" || type.value === "") {
+    return alert("Preencha todos os campos!");
+  }
 
-    items.push({
-        desc: descItem.value,
-        amount: Math.abs(amount.value).toFixed(2),
-        type: type.value,
-    });
+  items.push({
+    desc: descItem.value,
+    amount: Math.abs(amount.value).toFixed(2),
+    type: type.value,
+  });
 
-    setItensBD();
+  setItensBD();
 
-    loadItens();
+  loadItens();
 
-    descItem.value = "";
-    amount.value = "";
+  descItem.value = "";
+  amount.value = "";
 };
 
 function deleteItem(index) {
-    items.splice(index, 1);
-    setItensBD();
-    loadItens();
+  items.splice(index, 1);
+  setItensBD();
+  loadItens();
 }
 
 function insertItem(item, index) {
-    let tr = document.createElement("tr");
+  let tr = document.createElement("tr");
 
-    tr.innerHTML = `
+  tr.innerHTML = `
     <td>${item.desc}</td>
     <td>R$ ${item.amount}</td>
     <td class="columnType">${
-        item.type === "Entrada"
+      item.type === "Entrada"
         ? '<i class="bx bxs-chevron-up-circle"></i>'
         : '<i class="bx bxs-chevron-down-circle"></i>'
     }</td>
     <td class="columnAction">
-        <button onclick="deleteItem(${index})"><i class='bx-bx-trash'></i></button>
-        </td>
-    `;
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `;
 
-    tbody.appendChild(tr);
+  tbody.appendChild(tr);
 }
 
 function loadItens() {
-    items = getItensBD();
-    tbody.innerHTML = "";
-    items.forEach((item, index) => {
-        insertItem(item, index);
-    });
+  items = getItensBD();
+  tbody.innerHTML = "";
+  items.forEach((item, index) => {
+    insertItem(item, index);
+  });
 
-    getTotals();
+  getTotals();
 }
 
 function getTotals() {
-    const amountIncomes = items
-        .filter((item) => item.type === "Entrada")
-        .map((transaction) => Number(transaction.amount));
-    
-    const amountExpenses = items
-        .filter((item) => item.type === "Saída")
-        .map((transaction) => Number(transaction.amount));
-    
-    const totalIncomes = amountIncomes
-        .reduce((acc, cur) => acc + cur, 0)
-        .toFixed(2);
-    const totalExpenses = Math.abs(
-        amountExpenses.reduce((acc, cur) => acc + cur, 0)
-    ).toFixed(2);
+  const amountIncomes = items
+    .filter((item) => item.type === "Entrada")
+    .map((transaction) => Number(transaction.amount));
 
-    const totalItems = (totalIncomes - totalExpenses).toFixed(2);
+  const amountExpenses = items
+    .filter((item) => item.type === "Saída")
+    .map((transaction) => Number(transaction.amount));
 
-    incomes.innerHTML = totalIncomes;
-    expenses.innerHTML = totalExpenses;
-    total.innerHTML = totalItems;
+  const totalIncomes = amountIncomes
+    .reduce((acc, cur) => acc + cur, 0)
+    .toFixed(2);
+
+  const totalExpenses = Math.abs(
+    amountExpenses.reduce((acc, cur) => acc + cur, 0)
+  ).toFixed(2);
+
+  const totalItems = (totalIncomes - totalExpenses).toFixed(2);
+
+  incomes.innerHTML = totalIncomes;
+  expenses.innerHTML = totalExpenses;
+  total.innerHTML = totalItems;
 }
 
 const getItensBD = () => JSON.parse(localStorage.getItem("db_items")) ?? [];
-const setItensBD = () => localStorage.setItem("db_items", JSON.stringify(items));
+const setItensBD = () =>
+  localStorage.setItem("db_items", JSON.stringify(items));
 
 loadItens();
